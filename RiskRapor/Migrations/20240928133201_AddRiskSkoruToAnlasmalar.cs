2228452422a3cd67d32f1d45d4d031a40f450941
Kutay <toren.kutay@gmail.com>
2024-09-28 18:22:41 +0300

    risk analizi için db migration yapıldı view oluşturuldu

---
+++ RiskRapor/Migrations/20240928133201_AddRiskSkoruToAnlasmalar.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RiskRapor.Migrations
+{
+    public partial class AddRiskSkoruToAnlasmalar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
